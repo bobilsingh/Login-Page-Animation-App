4fd696f01996c3a0a2ddfa38b71ab8045a32b610
bobilsingh <bobilchauhan@yahoo.com>
2020-09-29 09:31:35 -0700

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/ALP_Bobil/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ALP_Bobil
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/searchWidget.dart
@@ -0,0 +1,25 @@
+import 'package:ALP_Bobil/components/searchTextfield.dart';
+import 'package:flutter/material.dart';
+
+class SearchWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 28),
+      child: Row(
+        children: <Widget>[
+          Expanded(child: SearchTextField()),
+          SizedBox(width: 11),
+          Container(
+            width: 70,
+            height: 60,
+            decoration: BoxDecoration(
+                color: Color(0xFFF1f94aa),
+                borderRadius: BorderRadius.circular(10)),
+            child: Icon(Icons.filter_list, color: Colors.white, size: 32),
+          )
+        ],
+      ),
+    );
+  }
+}
